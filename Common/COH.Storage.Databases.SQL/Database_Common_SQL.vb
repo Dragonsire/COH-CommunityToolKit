b0fb8c6c6c7c6b33ed8c464bb4204ed4ff3436a3
Mathew W <dragon.sire@hotmail.com>
2021-03-03 10:18:11 -0400

    Compiling Fixed, Ready to begin minor SQL testing

--- Common/COH.Storage.Databases.SQL/Database_Common_SQL.vb
+++ Common/COH.Storage.Databases.SQL/Database_Common_SQL.vb
@@ -28,30 +28,30 @@ Namespace Storage.Databases.SQL
 		Public Function OpenConnection() As Boolean Implements I_SupportDatabase_Accounts.OpenConnection
 			Dim i As Integer
 
-			MP_CREATE(asql_account, ACCOUNT_INITIAL_CONTAINER_SIZE)
-			MP_CREATE(asql_task, ACCOUNT_INITIAL_CONTAINER_SIZE)
+			'MP_CREATE(asql_account, ACCOUNT_INITIAL_CONTAINER_SIZE)
+			'MP_CREATE(asql_task, ACCOUNT_INITIAL_CONTAINER_SIZE)
 
-			Accounts = stashTableCreateInt(stashOptimalSize(ACCOUNT_INITIAL_CONTAINER_SIZE))
-			In_Flight = 0
+			'Accounts = stashTableCreateInt(stashOptimalSize(ACCOUNT_INITIAL_CONTAINER_SIZE))
+			'In_Flight = 0
 
-			sql_task_init(ASQL_WORKERS, 2048)
-			For i = 0 To ASQL_WORKERS - 1
-				sql_task_set_callback(i, asql_run_entry, DirectCast(CType(i + 1, IntPtr), Object))
-			Next i
-			sql_task_freeze()
+			'sql_task_init(ASQL_WORKERS, 2048)
+			'For i = 0 To ASQL_WORKERS - 1
+			'sql_task_set_callback(i, asql_run_entry, DirectCast(CType(i + 1, IntPtr), Object))
+			'Next i
+			'sql_task_freeze()
 
-			Debug.Assert(sqlConnInit(SQLCONN_MAX))
-			sqlConnSetLogin(g_accountServerState.cfg.sqlLogin)
+			'Debug.Assert(sqlConnInit(SQLCONN_MAX))
+			'sqlConnSetLogin(g_accountServerState.cfg.sqlLogin)
 
-			If Not sqlConnDatabaseConnect(g_accountServerState.cfg.sqlDbName, "ACCOUNTSERVER") Then
-				FatalErrorf("AccountServer could not connect to SQL database.")
-			End If
+			'If Not sqlConnDatabaseConnect(g_accountServerState.cfg.sqlDbName, "ACCOUNTSERVER") Then
+			'fatalErrorf("AccountServer could not connect to SQL database.")
+			'End If
 
-			For i = 0 To SQLCONN_MAX - 1
-				'C++ TO VB CONVERTER TODO TASK: The memory management function 'memset' has no equivalent in VB:
-				'C++ TO VB CONVERTER TODO TASK: There is no VB equivalent to 'sizeof':
-				memset(Connections(i).Procedures, 0, sizeof(Connections(i).proc))
-			Next i
+			'For i = 0 To SQLCONN_MAX - 1
+			'C++ TO VB CONVERTER TODO TASK: The memory management function 'memset' has no equivalent in VB:
+			'C++ TO VB CONVERTER TODO TASK: There is no VB equivalent to 'sizeof':
+			'memset(Connections(i).Procedures, 0, sizeof(Connections(i).proc))
+			'Next i
 		End Function
 #End Region
 
