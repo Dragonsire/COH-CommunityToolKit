08476b8f742bca8405a3f7fe9dcc42dfa2152228
Mathew W <dragon.sire@hotmail.com>
2021-03-06 13:13:41 -0400

    Database Options Added To Builder

---
+++ Common/COH.Storage.Databases.SQL/CodeManagement/Enums_SQL.vb
@@ -0,0 +1,36 @@
+﻿Namespace CodeManagement.Enums.Databases.SQL
+    Public Enum DatabaseOption_OnOffEnum
+        [ON]
+        OFF
+    End Enum
+    Public Enum DataBaseOptionsEnum
+        ANSI_NULL_DEFAULT
+        ANSI_NULLS
+        ANSI_PADDING
+        ANSI_WARNINGS
+        ARITHABORT
+        AUTO_CLOSE
+        AUTO_SHRINK
+        AUTO_UPDATE_STATISTICS
+        CURSOR_CLOSE_ON_COMMIT
+        CONCAT_NULL_YIELDS_NULL
+        NUMERIC_ROUNDABORT
+        QUOTED_IDENTIFIER
+        RECURSIVE_TRIGGERS
+        DISABLE_BROKER
+        AUTO_UPDATE_STATISTICS_ASYNC
+        DATE_CORRELATION_OPTIMIZATION
+        TRUSTWORTHY
+        ALLOW_SNAPSHOT_ISOLATION
+        READ_COMMITTED_SNAPSHOT
+        HONOR_BROKER_PRIORITY
+        'MULTI_USER
+        DB_CHAINING
+        'READ_WRITE
+        ''//
+        'CURSOR_DEFAULT '  Global 
+        'PARAMETERIZATION ' SIMPLE 
+        'RECOVERY ' FULL 
+        'PAGE_VERIFY ' CHECKSUM  
+    End Enum
+End Namespace
--- Common/COH.Storage.Databases.SQL/HelperFunctions/DataBase Builder/DatabaseBuilder.vb
+++ Common/COH.Storage.Databases.SQL/HelperFunctions/DataBase Builder/DatabaseBuilder.vb
@@ -7,37 +7,7 @@ Namespace HelperFunctions.Databases.SQL
         Public Property DatabaseName As String
         Public Property FileName As String
         Public Property Tables As List(Of DatabaseBuilder_DatabaseTable)
-        Public Property ANSI_NULL_DEFAULT As DatabaseOptionEnum
-        Public Property ANSI_NULLS As DatabaseOptionEnum
-        Public Property ANSI_PADDING As DatabaseOptionEnum
-        Public Property ANSI_WARNINGS As DatabaseOptionEnum
-        Public Property ARITHABORT As DatabaseOptionEnum
-        Public Property AUTO_CLOSE As DatabaseOptionEnum
-        Public Property AUTO_SHRINK As DatabaseOptionEnum
-        Public Property AUTO_UPDATE_STATISTICS As DatabaseOptionEnum
-        Public Property CURSOR_CLOSE_ON_COMMIT As DatabaseOptionEnum
-        Public Property CURSOR_DEFAULT As String ' Global 
-        Public Property CONCAT_NULL_YIELDS_NULL As DatabaseOptionEnum
-        Public Property NUMERIC_ROUNDABORT As DatabaseOptionEnum
-        Public Property QUOTED_IDENTIFIER As DatabaseOptionEnum
-        Public Property RECURSIVE_TRIGGERS As DatabaseOptionEnum
-        Public Property DISABLE_BROKER As DatabaseOptionEnum
-        Public Property AUTO_UPDATE_STATISTICS_ASYNC As DatabaseOptionEnum
-        Public Property DATE_CORRELATION_OPTIMIZATION As DatabaseOptionEnum
-        Public Property TRUSTWORTHY As DatabaseOptionEnum
-        Public Property ALLOW_SNAPSHOT_ISOLATION As DatabaseOptionEnum
-        Public Property PARAMETERIZATION As String 'SIMPLE 
-        Public Property READ_COMMITTED_SNAPSHOT As DatabaseOptionEnum
-        Public Property HONOR_BROKER_PRIORITY As DatabaseOptionEnum
-        Public Property RECOVERY As String ' FULL
-        Public Property MULTI_USER As DatabaseOptionEnum
-        Public Property PAGE_VERIFY As String 'CHECKSUM   
-        Public Property DB_CHAINING As DatabaseOptionEnum
-        Public Property READ_WRITE As DatabaseOptionEnum
-        Public Enum DatabaseOptionEnum
-            [ON]
-            OFF
-        End Enum
+        Public Property TheOptions As DatabaseBuilder_DatabaseOptions
 #End Region
 
 #Region "Create New Instance"
@@ -45,6 +15,7 @@ Namespace HelperFunctions.Databases.SQL
             DatabaseName = Name
             FileName = Location
             Tables = New List(Of DatabaseBuilder_DatabaseTable)
+            TheOptions = New DatabaseBuilder_DatabaseOptions
         End Sub
 #End Region
 
@@ -63,11 +34,7 @@ Namespace HelperFunctions.Databases.SQL
             Return SB.ToString
         End Function
         Public Function Export_CommandString_Options() As String
-            ''//ALTER DATABASE [cohacc] SET ANSI_NULL_DEFAULT OFF 
-            Dim SB As New Text.StringBuilder
-            SB.Append("ALTER DATABASE " & DatabaseName.BracketTheString & " SET ANSI_NULL_DEFAULT " & ANSI_NULL_DEFAULT.ToString)
-            SB.Append("ALTER DATABASE " & DatabaseName.BracketTheString & " SET ANSI_NULLS " & ANSI_NULLS.ToString)
-            Return SB.ToString
+            Return TheOptions.Export_CommandString_Options(DatabaseName)
         End Function
 #End Region
 
